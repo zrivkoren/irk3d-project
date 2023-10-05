2aeb6db298d2e29476fd2561e46c7556759a2d56
Denis Zavorovsky <zrivkoren@gmail.com>
2023-10-05 15:15:02 +0800

    Сделана кастомная страница 404

--- irk3d/config/urls.py
+++ irk3d/config/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+handler404 = 'tour.views.custom404'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
@@ -12,8 +14,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    # import debug_toolbar
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    # urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
--- irk3d/tour/views.py
+++ irk3d/tour/views.py
@@ -107,6 +107,10 @@ def home(request):
     return render(request, template, context)
 
 
+def custom404(request, exception):
+    return render(request, 'tour/404.html', status=404)
+
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     ip = x_forwarded_for.split(',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
